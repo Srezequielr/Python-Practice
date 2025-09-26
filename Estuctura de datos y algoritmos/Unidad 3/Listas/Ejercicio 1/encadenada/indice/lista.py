10177b70d698ec738bd974f00a922ab5500c20de
Julian <julianrierawrk@gmail.com>
2025-09-25 21:19:04 -0300

    listas encadenadas solo archivos

---
+++ Estuctura de datos y algoritmos/Unidad 3/Listas/Ejercicio 1/encadenada/indice/lista.py
@@ -0,0 +1,12 @@
+from nodo import Nodo
+
+class Lista:
+    __ult: int
+    __cabeza: Nodo
+    def __init__(self):
+        self.__cabeza = None
+        self.__ult = -1 
+    def insertar(self, pos, numero):
+        pass
+
+    
--- Estuctura de datos y algoritmos/Unidad 3/Listas/Ejercicio 1/encadenada/lista.py
+++
@@ -1,5 +0,0 @@
-from nodo import Nodo
-
-class Lista:
-    __cabeza: Nodo
-    
