e337501f5c1dbfa8a68dc188d9f05f64377774e4
Julian <julianrierawrk@gmail.com>
2025-10-14 10:42:13 -0300

    insertar arreglado

--- Estuctura de datos y algoritmos/Unidad 4/Prueba/arbol.py
+++ Estuctura de datos y algoritmos/Unidad 4/Prueba/arbol.py
@@ -19,16 +19,46 @@ class Arbol:
             print("Elemento ya existente")
         elif(nodo.getDato() < nuevaHoja.getDato()):
             print("Me desplace a la izquiera")
-            self.__insertar(nodo.getIzq(), dato)
+            if(nodo.getIzq() == None):
+                nodo.setIzq(nuevaHoja)
+                print("Inserte...")
+            else:
+                self.__insertar(nodo.getIzq(), dato)
         elif(nodo.getDato() > nuevaHoja.getDato()):
             print("Me desplace a la derecha")
-            self.__insertar(nodo.getDer(), dato)
+            if(nodo.getDer() == None):
+                nodo.setDer(nuevaHoja)
+                print("Inserte...")
+            else:
+                self.__insertar(nodo.getDer(), dato)
+    
     def getRaiz(self):
         return self.__raiz
 
 
+    def inorden (self,subarbol):
+        if subarbol != None:
+            self.inorden(subarbol.getIzq())
+            print(subarbol.getDato())
+            self.inorden(subarbol.getDer())
 
 
 
 
+
+if __name__ == "__main__":
+    arbol = Arbol()
+
+    arbol.insertar(70)
+    arbol.insertar(30)
+    arbol.insertar(80)
+    arbol.insertar(50)
+    arbol.insertar(75)
+    arbol.insertar(20)
+    xRaiz = arbol.getRaiz()
+    arbol.inorden(xRaiz)
+
+
+    
+            
 
