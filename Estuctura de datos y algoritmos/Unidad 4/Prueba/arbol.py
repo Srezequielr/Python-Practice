79c6699241b0625b4d6ed758104011119e353b7a
Julian <julianrierawrk@gmail.com>
2025-10-17 21:20:40 -0300

    arboles: insertar, recorrer, buscar, inOrder

--- Estuctura de datos y algoritmos/Unidad 4/Prueba/arbol.py
+++ Estuctura de datos y algoritmos/Unidad 4/Prueba/arbol.py
@@ -12,19 +12,16 @@ class Arbol:
 
     def __insertar(self, nodo, dato):
         nuevaHoja = Nodo(dato)
-        if(nodo == None):
-            print("Inserte...")
-            nodo = nuevaHoja
-        elif(nodo.getDato() == nuevaHoja.getDato()):
+        if(nodo.getDato() == nuevaHoja.getDato()):
             print("Elemento ya existente")
-        elif(nodo.getDato() < nuevaHoja.getDato()):
+        elif(nodo.getDato() > nuevaHoja.getDato()):
             print("Me desplace a la izquiera")
             if(nodo.getIzq() == None):
                 nodo.setIzq(nuevaHoja)
                 print("Inserte...")
             else:
                 self.__insertar(nodo.getIzq(), dato)
-        elif(nodo.getDato() > nuevaHoja.getDato()):
+        elif(nodo.getDato() < nuevaHoja.getDato()):
             print("Me desplace a la derecha")
             if(nodo.getDer() == None):
                 nodo.setDer(nuevaHoja)
@@ -35,28 +32,48 @@ class Arbol:
     def getRaiz(self):
         return self.__raiz
 
+    def posOrden(self):
+        pass
 
-    def inorden (self,subarbol):
+    def preOrden(self):
+        pass
+
+    def inOrden (self,subarbol):
         if subarbol != None:
-            self.inorden(subarbol.getIzq())
+            self.inOrden(subarbol.getIzq())
             print(subarbol.getDato())
-            self.inorden(subarbol.getDer())
+            self.inOrden(subarbol.getDer())
+
+    def suprimir(sels):
+        pass
+
+    def vacio(self):
+        return self.getRaiz() == None
+    
+    def esHoja(self, subArbol):
+        return subArbol.getIzq() == None and subArbol.getDer() == None
+
+    def buscar(self, subArbol, x):
+        if(self.vacio()):
+            print("El arbol esta vacio")
+        else:
+            if(subArbol == None):
+                print("No se encuentro el elemento")
+            elif(x == subArbol.getDato()):      
+                print(f"Encontro el elemento: {subArbol.getDato()}")  
+                return subArbol
+            elif(x < subArbol.getDato()):
+                print("Mi x es menor, me voy a la izquierda")
+                return self.buscar(subArbol.getIzq(), x)
+            else:
+                print("Mi x es mayor, me voy a la derecha.")
+                return self.buscar(subArbol.getDer(), x)
+    
+    def suprimir(self, elem):
+        if(self.buscar(elem) == None):
+            print("Elemento inexistente.")
 
 
-
-
-
-if __name__ == "__main__":
-    arbol = Arbol()
-
-    arbol.insertar(70)
-    arbol.insertar(30)
-    arbol.insertar(80)
-    arbol.insertar(50)
-    arbol.insertar(75)
-    arbol.insertar(20)
-    xRaiz = arbol.getRaiz()
-    arbol.inorden(xRaiz)
 
 
     
--- Estuctura de datos y algoritmos/Unidad 4/Prueba/main.py
+++ Estuctura de datos y algoritmos/Unidad 4/Prueba/main.py
@@ -3,13 +3,18 @@ from arbol import Arbol
 def main():
     arbol = Arbol()
 
-    arbol.insertar(20)
+    arbol.insertar(85)
     arbol.insertar(30)
     arbol.insertar(40)
-    arbol.insertar(50)
-    arbol.insertar(60)
-    arbol.insertar(70)
+    arbol.insertar(5)
+    arbol.insertar(34)
+    arbol.insertar(12)
+    
+    xRaiz = arbol.getRaiz()
 
+    arbol.inOrden(xRaiz)
+
+    print(arbol.buscar(xRaiz, 5))
 
 if __name__ == "__main__":
     main()
